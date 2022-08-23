1b1036739ba4cc434a2036574311e79087713962
Florian R <47738387+FlorianROUVIER@users.noreply.github.com>
2022-08-23 20:08:32 +0200

    Initial commit

---
+++ src/pages/Home.tsx
@@ -0,0 +1,12 @@
+import { Container } from "react-bootstrap";
+
+export function Home() {
+  return (
+    <>        
+      <Container>
+        <h1>Home</h1>
+        <div>Placeholder</div>
+      </Container>
+    </>
+  );
+}
